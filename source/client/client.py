961cc87b2060a3df453e3c39c22ff387fcc2c03f
Pierre <pierrelissope@gmail.com>
2024-06-23 17:39:04 +0200

    feat(client): added client processing main loop

---
+++ source/client/client.py
@@ -0,0 +1,33 @@
+import socket
+import sys
+import select
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 12345
+BUFFER_SIZE = 1024
+
+def send_message(socket, message):
+    message_header = f"{len(message):<{BUFFER_SIZE}}"
+    socket.send(bytes(message_header, 'utf-8') + message)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((SERVER_HOST, SERVER_PORT))
+
+name = input("Entrez votre nom: ")
+send_message(client_socket, bytes(name, 'utf-8'))
+
+while True:
+    sockets_list = [sys.stdin, client_socket]
+    read_sockets, _, _ = select.select(sockets_list, [], [])
+
+    for notified_socket in read_sockets:
+        if notified_socket == client_socket:
+            message = client_socket.recv(BUFFER_SIZE)
+            if not message:
+                print('Déconnexion du serveur')
+                sys.exit()
+            else:
+                print(message.decode('utf-8'))
+        else:
+            message = sys.stdin.readline().strip()
+            send_message(client_socket, bytes(message, 'utf-8'))
