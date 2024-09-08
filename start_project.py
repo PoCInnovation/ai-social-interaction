63646d27a79b7f70a9549206bc61fe4825e04f37
Laiheau <leo.barbier@epitech.eu>
2024-09-08 17:36:50 +0200

    feat: starting script

---
+++ start_project.py
@@ -0,0 +1,23 @@
+from sys import argv
+import os
+import time
+
+
+if len(argv) < 3:
+    print("please precise a number of client and second, ex : python start_project.py 5 10, for 5 client running for 10 seconds")
+    exit(0)
+try:
+    int(argv[1])
+except ValueError:
+    print("please enter a valid value for the number of client")
+try:
+    int(argv[2])
+except ValueError:
+    print("please enter a valid value for the number of seconds")
+    exit(0)
+os.system("./main server &")
+for i in range(int(argv[1])):
+    os.system("./main client &")
+time.sleep(int(argv[2]))
+os.system('pkill -f "python ./main client"')
+os.system('pkill -f "python ./main server"')
