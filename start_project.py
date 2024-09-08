4d7fc4502b3aafbbc7bf94e9b8e3da8fe4bdf11e
Laiheau <leo.barbier@epitech.eu>
2024-09-08 17:39:01 +0200

    fix: another error in starting script

--- start_project.py
+++ start_project.py
@@ -5,17 +5,17 @@ import time
 
 if len(argv) < 3:
     print("please precise a number of client and second, ex : python start_project.py 5 10, for 5 client running for 10 seconds")
-    exit(0)
+    exit(1)
 try:
     int(argv[1])
 except ValueError:
     print("please enter a valid value for the number of client")
-    exit(0)
+    exit(1)
 try:
     int(argv[2])
 except ValueError:
     print("please enter a valid value for the number of seconds")
-    exit(0)
+    exit(1)
 os.system("./main server &")
 for i in range(int(argv[1])):
     os.system("./main client &")
