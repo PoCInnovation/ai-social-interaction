71615a102b9dfef025ee1790528fb8334395c58a
LixiosDelios <alexandre.guillaud@epitech.eu>
2024-09-08 15:23:36 +0200

    remove : useless import of doteenv in server.py

--- source/server/server.py
+++ source/server/server.py
@@ -8,9 +8,6 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from config import DataConfig
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Server:
 
